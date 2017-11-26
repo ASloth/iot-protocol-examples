5a77a603202270866f106df8cece0ac11bcb6b21
Konrad Müller <k.l.mueller@outlook.com>
2017-11-26 14:08:09 +0100

    Added coap publisher

---
+++ coap/publisher/index.js
@@ -0,0 +1,29 @@
+const coap = require('coap'), // or coap
+    request = require('request'),
+    localObserver = coap.request({
+        observe: true,
+        host: 'localhost',
+        port: 6003
+    }),
+    remoteServer = ""; //Set the ip of the server where the backend is running here
+
+localObserver.on('response', function (res) {
+    sendDataToRemote(res);
+});
+
+function sendDataToRemote(res)
+{
+    console.log(res);
+    res.setEncoding('utf8');
+    res.on('data', function (data) {
+        var obj = JSON.parse(data.toString());
+        console.log("Recieved value (" + obj.value + ") of type " + obj.valueType + " from " + obj.sensorName + " (" + obj.sensorId + ")");
+        var r = request(remoteServer, {
+            method: 'POST',
+            body: obj,
+            json: true
+        });
+    });
+}
+
+localObserver.end(); 
