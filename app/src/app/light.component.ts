0f3e3589857db0b54c36b3e0f383d9b37853c1e3
KrdMlr <ASloth@users.noreply.github.com>
2017-12-05 18:54:45 +0100

    spelling

--- app/src/app/light.component.ts
+++ app/src/app/light.component.ts
@@ -15,7 +15,7 @@ export class LightGraphComponent implements OnInit, OnDestroy, AfterViewInit {
 
   // lineChart
   public lineChartData: Array<any> = [
-    {data: [], label: 'Light Intensity'}
+    {data: [], label: 'Lichtstärke'}
   ];
   public lineChartLabels: Array<any> = [];
   public lineChartOptions: any = {
